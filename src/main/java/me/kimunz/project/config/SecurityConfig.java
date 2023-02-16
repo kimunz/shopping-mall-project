5057f79cc9aae3501595a65e0cbe90d53c15b0b2
Eunji Kim <oijkt7@gmail.com>
2023-02-17 00:06:43 +0900

    update config file

--- src/main/java/me/kimunz/project/config/SecurityConfig.java
+++ src/main/java/me/kimunz/project/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package me.kimunz.project.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity //기본적인 web 보안 활성화
 public class SecurityConfig {
+
     //WebSecurityConfigurerAdapter는 더 이상 사용되지 않는 클래스
+    //아래처럼 빈으로 등록해줘야함
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -15,5 +18,11 @@ public class SecurityConfig {
                 .antMatchers("/api/hello").permitAll() //이 요청은 인증없이 접근 허용
                 .anyRequest().authenticated(); //나머지 요청에 대해서는 인증을 받아야 함
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .antMatchers("/h2-console/**","/favicon.ico");
     }
 }
