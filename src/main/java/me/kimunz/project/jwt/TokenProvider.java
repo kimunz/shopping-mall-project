9a3fb16fce92560bd7990f90c9b9890b25402648
Eunji Kim <oijkt7@gmail.com>
2023-02-17 00:08:51 +0900

    update token provider file

---
+++ src/main/java/me/kimunz/project/jwt/TokenProvider.java
@@ -0,0 +1,38 @@
+package me.kimunz.project.jwt;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+
+@Component
+public class TokenProvider implements InitializingBean {
+// 스프링 빈 초기화 시 특정작업을 함
+    private final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
+
+    private static final String AUTHORITIES_KEY = "auth";
+
+    private final String secret;
+    private final long tokenValidityInMilliseconds;
+
+    private Key key;
+    public TokenProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds) {
+        this.secret = secret;
+        this.tokenValidityInMilliseconds = tokenValidityInSeconds;
+    }
+    // 빈이 생성되고 의존성을 주입받은 후
+    // secret값을 Base64 Decode를 해서 key변수에 할당하기 위함
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+}
