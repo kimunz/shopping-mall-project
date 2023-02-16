7f894e59ae60e0a0a867125cb52f4c57909a9829
Eunji Kim <oijkt7@gmail.com>
2023-02-17 00:07:52 +0900

    update sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into "USER" (name, username, password, email, phone, address, auth)
+values ('관리자', 'admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin@test.com', '010-0000-0000', '없음', 'ROLE_ADMIN');
+insert into "USER" (name, username, password, email, phone, address, auth)
+values ('홍길동', 'user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user@test.com', '010-0000-0001', '서울', 'ROLE_USER');
+
