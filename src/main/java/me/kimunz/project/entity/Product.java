800ab70717ab73e6ce04fe4d0b0d5e3a206e3746
Eunji Kim <oijkt7@gmail.com>
2023-03-05 22:03:21 +0900

    update Product mapper

--- src/main/java/me/kimunz/project/entity/Product.java
+++ src/main/java/me/kimunz/project/entity/Product.java
@@ -1,4 +1,21 @@
 package me.kimunz.project.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+
+    private Long id;
+    private String category;
+    private String name;
+    private String description;
+    private int stock;
+    private int price;
+
 }
